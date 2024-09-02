edf947d6a18b49bc6f6aceb49e4082c4f10d6ccc
Florents Tselai <florents.tselai@gmail.com>
2024-09-02 18:06:02 +0300

    more onnx tests

--- test/sql/embed_onnx.sql
+++ test/sql/embed_onnx.sql
@@ -1,1 +1,4 @@
 select array_length(llm_embed('hello world', 'onnx-bge-micro'), 1);
+select array_length(llm_embed('hello world', 'onnx-gte-tiny'), 1);
+select array_length(llm_embed('hello world', 'onnx-minilm-l6'), 1);
+select array_length(llm_embed('hello world', 'onnx-minilm-l12'), 1);
